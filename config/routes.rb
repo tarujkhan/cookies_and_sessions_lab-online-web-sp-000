4ccccbc35ece54604ddc7bed879559a8cb868eb8
taroj khan <tarujkhan@yahoo.com>
2021-03-01 18:21:03 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details  Rails.application.routes.draw do
+  For details  Rails.application.routes.draw do
 
       resources :products, only: [:index, :add]
     end

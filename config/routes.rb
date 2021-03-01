79dd9debd98af9a4c127d04440ee219035c5d4bf
taroj khan <tarujkhan@yahoo.com>
2021-03-01 18:20:52 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # For details  Rails.application.routes.draw do
+
+      resources :products, only: [:index, :add]
+    end
 end
